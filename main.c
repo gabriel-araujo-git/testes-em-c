db855605aad5574b64b5c0f98d79de75b5e2718f
Gabriel Araujo <86240279+Usuario73@users.noreply.github.com>
2023-03-16 07:57:00 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+
+    int idade = 0;
+
+    printf("o valor inicial da idade é: %d.\n", idade);
+
+    printf("digite uma idade:\n");
+    scanf("%d", &idade);
+
+    return 0;
+}
